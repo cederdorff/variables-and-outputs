1b845592f9da570ed5b510f54b4b09004811792f
Cg69-hash <cgskak@hotmail.com>
2023-02-23 13:14:08 +0100

    jA

--- app.js
+++ app.js
@@ -7,28 +7,39 @@ let number = 0;
 // ========== Init App Event ========== //
 
 function initApp() {
-    number = 0;
-    document.querySelector("#btn-increment").addEventListener("click", incrementNumber);
-    document.querySelector("#btn-decrement").addEventListener("click", decrementNumber);
+  number = 0;
+  document
+    .querySelector("#btn-increment")
+    .addEventListener("click", incrementNumber);
+  document
+    .querySelector("#btn-decrement")
+    .addEventListener("click", decrementNumber);
 }
 
 // ========== Increment and decrement functions ========== //
 
 function incrementNumber() {
-    number = number + 1;
-    displayNumber();
+  number = number + 1;
+  displayNumber();
 }
 
 function decrementNumber() {
-    number = number - 1;
-    displayNumber();
+  number = number - 1;
+  displayNumber();
 }
 
 // ========== Display Number function ========== //
 
 function displayNumber() {
-    document.querySelector("#number").textContent = number;
-    document.querySelector("#message").textContent = `The number is: ${number}`;
-    // document.querySelector("#message").textContent = "The number is:" + number;
+  document.querySelector("#number").textContent = number;
+  let message = "";
+  if (number > 10) {
+    message = "Number is above 10";
+  } else if (number == 10) {
+    message = "Number is 10";
+  } else {
+    message = "Number is below 10";
+  }
+  console.log(message);
+  document.querySelector("#message").textContent = message;
 }
-
